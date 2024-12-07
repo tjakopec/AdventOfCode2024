c47c549e11a0c50dac4dcee615dade6acc49de46
Tomislav Jakopec <tjakopec@gmail.com>
2024-12-07 07:21:19 +0100

    Day 3 part 2

--- AdventOfCode2024/T03.cs
+++ AdventOfCode2024/T03.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.IO;
 using System.Text;
 
@@ -13,11 +14,35 @@ namespace AdventOfCode2024
             Console.WriteLine("Day 3");
 			LoadData();
             Console.WriteLine(CalculateFirstPart());
-            //Console.WriteLine(CalculateSecondPart());
+            Console.WriteLine(CalculateSecondPart());
             Console.WriteLine("-------------------");
         }
 
-        
+        private int CalculateSecondPart()
+        {
+
+            StringBuilder sb = new StringBuilder();
+            string[] partsDont = Data.Split("don't()");
+            string[] partsDo;
+            sb.Append(partsDont[0]);
+
+            for(int i = 1; i < partsDont.Length; i++)
+            {
+               
+                    partsDo = partsDont[i].Split("do()");
+                    for(int j = 1; j < partsDo.Length; j++)
+                    {
+                        sb.Append(partsDo[j]);
+                    }
+                    
+               
+            }
+
+            Data = sb.ToString();
+            return CalculateFirstPart();
+        }
+
+
 
         private int CalculateFirstPart()
         {
